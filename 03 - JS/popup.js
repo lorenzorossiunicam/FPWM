b4f3781a0c4287c6a6e0fafbc86e10366188af90
lorenzorossiunicam <lorenzo.rossi@unicam.it>
2020-09-27 14:18:26 +0200

    =% - JS

--- 03 - JS/popup.js
+++ 03 - JS/popup.js
@@ -1,9 +1,11 @@
+
+
 function mostra() {
   var x = document.getElementById("pop");
   x.style.display = "block";
 }
 
-var x = document.getElementById("pop");
+
 x.addEventListener ('click', function(){
   x.style.display = "none";
 });
